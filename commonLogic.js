de09938db295a878548c69e75e9e7bd2f7bb3335
Lucas de Almeida <lucasdealmeidasm@gmail.com>
2014-10-22 02:18:12 -0200

    Created Projectile base class

--- commonLogic.js
+++ commonLogic.js
@@ -135,48 +135,6 @@ function ExistentAndAlive(obj)
 	return true;
 }
 
-function ProjectileUpdate()
-{
-	if((this.frameDeath) && (frame == this.frameDeath))
-	{
-		alive = false;
-		return;
-	}
-	this.x += this.dx;
-	this.y += this.dy;
-	// Why two loops? So that objects spawned in positions they would die instantly do no harm (PRIMVM NON NOCERE)
-	for(var i = 0; i < entities.length; i++)
-	{
-		var obj = entities[i];
-		if(!obj.alive) continue;
-		if(obj == this) continue;
-		
-		if(Collision(this, obj))
-		{
-			if((obj.IsSolidTo) && (obj.IsSolidTo(this)))
-			{
-				this.alive = false;
-				return;
-			}
-		}
-	}
-	for(var i = 0; i < entities.length; i++)
-	{
-		var obj = entities[i];
-		if(!obj.alive) continue;
-		if(obj == this) continue;
-		
-		if(Collision(this, obj))
-		{
-			if(obj.Hit == this.targetHit)
-			{
-				obj.Hit(this.damage);
-				this.alive = false;
-			}
-		}
-	}
-}
-
 function BackAndForthUpdate()
 {
 	if(this.dx > 0)
---
+++ projectile.js
@@ -0,0 +1,45 @@
+function Projectile()
+{
+}
+
+Projectile.prototype.Update = function()
+{
+	if((this.frameDeath) && (frame == this.frameDeath))
+	{
+		alive = false;
+		return;
+	}
+	this.x += this.dx;
+	this.y += this.dy;
+	// Why two loops? So that objects spawned in positions they would die instantly do no harm (PRIMVM NON NOCERE)
+	for(var i = 0; i < entities.length; i++)
+	{
+		var obj = entities[i];
+		if(!obj.alive) continue;
+		if(obj == this) continue;
+		
+		if(Collision(this, obj))
+		{
+			if((obj.IsSolidTo) && (obj.IsSolidTo(this)))
+			{
+				this.alive = false;
+				return;
+			}
+		}
+	}
+	for(var i = 0; i < entities.length; i++)
+	{
+		var obj = entities[i];
+		if(!obj.alive) continue;
+		if(obj == this) continue;
+		
+		if(Collision(this, obj))
+		{
+			if(obj.Hit == this.targetHit)
+			{
+				obj.Hit(this.damage);
+				this.alive = false;
+			}
+		}
+	}
+}
