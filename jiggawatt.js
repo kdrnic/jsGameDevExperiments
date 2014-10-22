87f2952b9b117854cff977238bdbbae22d1ac520
Lucas de Almeida <lucasdealmeidasm@gmail.com>
2014-10-22 01:49:41 -0200

    Moved methods to prototype

--- jiggawatt.js
+++ jiggawatt.js
@@ -8,30 +8,30 @@ function Jiggawatt()
 	this.alive = true;
 	
 	this.Draw = ImageDraw;
-	
-	this.Update = function ()
+}
+
+Jiggawatt.prototype.Update = function ()
+{
+	var dx = 0, dy = 0;
+	if(keys["keyLeft"].state != 0) dx = -1;
+	if(keys["keyRight"].state != 0) dx = 1;
+	if(keys["keyUp"].state != 0) dy = -1;
+	if(keys["keyDown"].state != 0) dy = 1;
+	var dl = Math.sqrt((dx * dx) + (dy * dy));
+	if(dl != 0)
 	{
-		var dx = 0, dy = 0;
-		if(keys["keyLeft"].state != 0) dx = -1;
-		if(keys["keyRight"].state != 0) dx = 1;
-		if(keys["keyUp"].state != 0) dy = -1;
-		if(keys["keyDown"].state != 0) dy = 1;
-		var dl = Math.sqrt((dx * dx) + (dy * dy));
-		if(dl != 0)
-		{
-			dx *= this.speed / dl;
-			dy *= this.speed / dl;
-		}
-		
-		this.x += dx;
-		this.y += dy;
-		AvoidSolids(this);
-	
-		CameraFocusOn(this);
+		dx *= this.speed / dl;
+		dy *= this.speed / dl;
 	}
 	
-	this.Hit = function(dmg)
-	{
-		this.health -= dmg;
-	}
+	this.x += dx;
+	this.y += dy;
+	AvoidSolids(this);
+
+	CameraFocusOn(this);
+}
+
+Jiggawatt.prototype.Hit = function(dmg)
+{
+	this.health -= dmg;
 }
