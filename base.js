f697e46fc85e47f4baa28a2fe1dd9d37f4004af8
Lucas de Almeida <lucasdealmeidasm@gmail.com>
2014-10-22 01:00:29 -0200

    First commit

---
+++ base.js
@@ -0,0 +1,122 @@
+var pressed = [];
+var entities = [];
+var camera = {};
+var canvas;
+var context;
+var frame;
+var mouse = {};
+var preventDefault = false;
+var freeze;
+
+function KeyDownEvent(event)
+{
+	pressed[event.keyCode] = true;
+	if((!event.shiftKey) && (!event.ctrlKey) && (!event.altKey) && (!event.metaKey) && (preventDefault)) event.preventDefault();
+}
+
+function KeyUpEvent(event)
+{
+	pressed[event.keyCode] = false;
+}
+
+function MouseMoveEvent(event)
+{
+	mouse.x = event.x - canvas.offsetLeft;
+	mouse.y = event.y - canvas.offsetTop;
+}
+
+function MouseUpEvent(event)
+{
+	mouse.pressed[event.button] = false;
+}
+
+function MouseDownEvent(event)
+{
+	mouse.pressed[event.button] = true;
+}
+
+function Start()
+{
+	window.addEventListener("keyup", KeyUpEvent);
+	window.addEventListener("keydown", KeyDownEvent);
+	canvas = document.getElementById("canvas");
+	mouse.pressed = [];
+	canvas.addEventListener("mousemove", MouseMoveEvent);
+	canvas.addEventListener("mousedown", MouseDownEvent);
+	canvas.addEventListener("mouseup", MouseUpEvent);
+	context = canvas.getContext("2d");
+	frame = 0;
+	camera.x = 0;
+	camera.y = 0;
+	StartLoadingData();
+	freeze = true;
+	setTimeout("DoFrame()", 1000.0 / 60.0);
+}
+
+function NewEntity()
+{
+	for(var i = 0; i < entities.length; i++)
+	{
+		if(!entities[i].alive)
+		{
+			entities[i] = {};
+			entities[i].alive = true;
+			return entities[i];
+		}
+	}
+	entities[entities.length] = {};
+	entities[entities.length - 1].alive = true;
+	return entities[entities.length - 1];
+}
+
+function ClearEntities()
+{
+	entities = [];
+}
+
+function DoFrame()
+{
+	Update();
+	Draw();
+	if(!freeze) frame++;
+	setTimeout("DoFrame()", 1000.0 / 60.0);
+}
+
+function Update()
+{
+	if(!freeze) UpdateEntities();
+}
+
+function UpdateEntities()
+{
+	for(var i = 0; i < entities.length; i++)
+	{
+		var entity = entities[i];
+		if(!entity.alive) continue;
+		if(entity.Update) entity.Update();
+	}
+}
+
+function Draw()
+{
+	DrawEntities();
+}
+
+function CompareLayers(a, b)
+{
+	var layerA = 0, layerB = 0;
+	if(a.layer) layerA = a.layer;
+	if(b.layer) layerB = b.layer;
+	return layerA - layerB;
+}
+
+function DrawEntities()
+{
+	if(frames % 10 == 0) entities.sort(CompareLayers); // Sorts every 10 frames for greater speed
+	for(var i = 0; i < entities.length; i++)
+	{
+		var object = entities[i];
+		if(!object.alive) continue;
+		if(object.Draw) object.Draw();
+	}
+}
---
+++ data.js
@@ -0,0 +1,42 @@
+// Array of strings containing the URLs for each data to be preloaded
+var dataSrcs =
+[
+	"gfx/grass1.png",
+	"gfx/grass2.jpg",
+	"gfx/grass3.jpg"
+];
+var data = {};
+
+function StartLoadingData()
+{
+	data.length = 0;
+	for(var i = 0; i < dataSrcs.length; i++)
+	{
+		var extension = dataSrcs[i].substr(dataSrcs[i].length - 4, 4);
+		if((extension == ".png") || (extension == ".jpg") || (extension == ".bmp") || (extension == ".gif"))
+		{
+			var img = new Image();
+			img.shortSrc = dataSrcs[i];
+			img.src = dataSrcs[i];
+			img.onload = function()
+			{
+				data[this.shortSrc] = this;
+				data.length++;
+				if(typeof OnMoreData != undefined) OnMoreData();
+			}
+		}
+		if((extension == ".wav") || (extension == ".ogg"))
+		{
+			var snd = new Audio();
+			snd.shortSrc = dataSrcs[i];
+			snd.src = dataSrcs[i];
+			snd.load();
+			snd.oncanplaythrough = function()
+			{
+				data[this.shortSrc] = this;
+				data.length++;
+				if(typeof OnMoreData != undefined) OnMoreData();
+			}
+		}
+	}
+}
